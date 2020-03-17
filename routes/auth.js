a366973759845fd65dc126c930a4c38e21209804
Valentin <43175508+V2i@users.noreply.github.com>
2020-03-17 18:23:31 +0100

    password hashing and checking if user already exists

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 const  {registerValidation} = require('../validation/register');
+const {loginValidation} = require('../validation/login');
 
 router.post('/register', async (req, res) => {
 
@@ -9,13 +11,27 @@ router.post('/register', async (req, res) => {
     const {error} =  registerValidation(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
+    //checking if user already exist
+    try {
+        const emailExist = await User.findOne({userMail : req.body.userMail});
+        if(emailExist) return res.status(400).send('Email already exists');
+    } catch (err) {
+        res.status(400).send(err);
+    }
+
+    //hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.userPassword, salt);
+
+
+
     //user creation
     const user = new User({
         userName: req.body.userName,
         userFirstName: req.body.userFirstName,
         userPseudo: req.body.userPseudo,
         userMail: req.body.userMail,
-        userPassword: req.body.userPassword,
+        userPassword: hashedPassword,
         isAdmin: false,
         isPrivate: false,
         isBan: false
@@ -23,11 +39,15 @@ router.post('/register', async (req, res) => {
 
     //posting to db
     try {
-        const savedUser = await user.save();
-        await res.json(savedUser);
+        await user.save();
+        await res.json({user: user._id});
     } catch (err) {
         res.status(400).send(err);
     }
+
+});
+
+router.post('/login', async (req, res) => {
 
 });
 
