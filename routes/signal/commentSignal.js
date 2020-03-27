89e0af73f8e3c5115e1e84d585e8ebbd25817aa3
GuillaumeClement <guillaume.clement@etu.umontpellier.Fr>
2020-03-27 18:11:16 +0100

    Signal Comment

--- routes/signal/commentSignal.js
+++ routes/signal/commentSignal.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const Comment = require('../../models/Comment');
 
 router.put('/:idCom', function (req, res) {
-    Comment.findById(req.params.idCom).then(
-        comment => {
-            if (!comment.reports.contains(req.user._id)){
-                Comment.findByIdAndUpdate(comment._id, {$push :{reports: req.user._id}})
-            }
-        }
-    )
+	Comment.findById(req.params.idCom).then(
+		comment => {
+			if (!comment.reports.includes(req.user._id)) {
+				Comment.findByIdAndUpdate(comment._id, {$push: {reports: req.user._id}})
+					.then(data => res.status(201).json(data))
+					.catch(err => res.status(500).json(err))
+			} else {
+        res.status(500).end()
+      }
+		}
+	)
+		.catch(err => res.status(500).json(err))
 });
 
 module.exports = router;
