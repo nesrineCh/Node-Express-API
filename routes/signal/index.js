744c50f43602cc6c2eb82aa4b9a3af2b0355a8e5
GuillaumeClement <guillaume.clement@etu.umontpellier.Fr>
2020-03-27 16:40:06 +0100

    Voila

--- routes/signal/index.js
+++ routes/signal/index.js
@@ -20,11 +20,6 @@ router.get('/', async function (req, res) {
 		const listCommentSig = await Comment.find({reports: {$exists: true, $ne: []}});
 		const listPostSig = await Publication.find({reports: {$exists: true, $ne: []}});
 
-
-		// Todo : get signals from Publication et Comment
-
-		// const res = await Promise.all([CommentSignal.find(), PubSignal.find()]);
-
 		res.status(200).send({listCommentSig, listPostSig});
 
 	} catch (e) {
