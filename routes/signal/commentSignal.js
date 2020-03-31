f18369ddae26804afcfb4922f3257280c68534e8
GuillaumeClement <guillaume.clement@etu.umontpellier.Fr>
2020-03-31 18:20:42 +0200

    Fixed bug and added delete signal route

--- routes/signal/commentSignal.js
+++ routes/signal/commentSignal.js
@@ -10,11 +10,39 @@ router.put('/:idCom', function (req, res) {
 					.then(data => res.status(201).json(data))
 					.catch(err => res.status(500).json(err))
 			} else {
-                res.status(200).end()
-            }
+				res.status(400).send({error: "Already reported"})
+			}
 		}
 	)
-		.catch(err => res.status(500).json(err))
+		.catch(err => res.status(400).json(err))
+});
+
+// delete un signal
+router.delete('/:idCom', function (req, res) {
+	if (!req.user) {
+		return res.status(403).end()
+	}
+
+	Comment.findById(req.params.idCom).then(
+		publication => {
+
+			if (publication.reports.includes(req.user._id)) {
+				let id = req.user._id;
+				Comment.findByIdAndUpdate(publication._id, {$pull: {reports: id}})
+					.then(data => res.status(200).json(data))
+					.catch(err => res.status(500).json(err))
+
+			} else if (req.user.isAdmin && res.body.idUser) {
+
+				Comment.findByIdAndUpdate(publication._id, {$pull: {reports: res.body.idUser}})
+					.then(data => res.status(200).json(data))
+					.catch(err => res.status(500).json(err))
+
+			} else {
+				res.status(400).end()
+			}
+		}
+	).catch(err => res.status(400).json(err))
 });
 
 module.exports = router;
--- routes/signal/pubSignal.js
+++ routes/signal/pubSignal.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const Publication = require('../../models/Publication');
 
 router.put('/:idPub', function (req, res) {
+	if (!req.user) {
+		return res.status(403).end()
+	}
 	Publication.findById(req.params.idPub).then(
 		publication => {
 			if (!publication.reports.includes(req.user._id)) {
@@ -10,10 +13,38 @@ router.put('/:idPub', function (req, res) {
 					.then(data => res.status(201).json(data))
 					.catch(err => res.status(500).json(err))
 			} else {
-				res.status(200).end()
+				res.status(400).send({error: "Already reported"})
 			}
 		}
-	).catch(err => res.status(500).json(err))
+	).catch(err => res.status(400).json(err))
+});
+
+// delete un signal
+router.delete('/:idPub', function (req, res) {
+	if (!req.user) {
+		return res.status(403).end()
+	}
+
+	Publication.findById(req.params.idPub).then(
+		publication => {
+
+			if (publication.reports.includes(req.user._id)) {
+				let id = req.user._id;
+				Publication.findByIdAndUpdate(publication._id, {$pull: {reports: id}})
+					.then(data => res.status(200).json(data))
+					.catch(err => res.status(500).json(err))
+
+			} else if (req.user.isAdmin && res.body.idUser) {
+
+				Publication.findByIdAndUpdate(publication._id, {$pull: {reports: res.body.idUser}})
+					.then(data => res.status(200).json(data))
+					.catch(err => res.status(500).json(err))
+
+			} else {
+				res.status(400).end()
+			}
+		}
+	).catch(err => res.status(400).json(err))
 });
 
 module.exports = router;
